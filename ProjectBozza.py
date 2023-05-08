7b0b0fc32704c02f1b9b7b60316334a088b3e88e
PetraBattistata <127765772+PetraBattistata@users.noreply.github.com>
2023-05-08 17:16:09 +0200

    Update ProjectBozza.py

--- ProjectBozza.py
+++ ProjectBozza.py
@@ -9,8 +9,12 @@ st.title('Counter Example')
 if 'count' not in st.session_state:
     st.session_state.count = 3
 
+    #agg_ita1
+    
 increment = st.button('Increment')
 if increment:
     st.session_state.count -= 1
 
 st.write('Count = ', st.session_state.count)
+if st.session_state.count <= 0:
+    break
