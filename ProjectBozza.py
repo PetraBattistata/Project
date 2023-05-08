a2282f2317366d77d2a01e9721d6cd74384c1b5e
PetraBattistata <127765772+PetraBattistata@users.noreply.github.com>
2023-05-08 17:43:36 +0200

    Update ProjectBozza.py

--- ProjectBozza.py
+++ ProjectBozza.py
@@ -9,12 +9,15 @@ if 'count' not in st.session_state:
     st.session_state.count = 3
 
     #agg_ita1
+
     
+if st.session_state.count <= 0:
+    st.stop()
 increment = st.button('Increment')
 if increment:
-    st.session_state.count -= 1
+    st.session_state.count -= 1 
 
 st.write('Count = ', st.session_state.count)
 if st.session_state.count <= 0:
     st.write('Game over!')
-    pyautogui.hotkey('ctrl', 'F5')
+
