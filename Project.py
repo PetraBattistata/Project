0e0bf2c541ab02fb3ec34404b092db75c91900f7
PetraBattistata <127765772+PetraBattistata@users.noreply.github.com>
2023-05-08 16:35:08 +0200

    Update Project.py

--- Project.py
+++ Project.py
@@ -4,6 +4,8 @@ import random
 from googletrans import Translator
 
 Counter1 = 3
+if 'Counter1' not in st.session_state:
+    st.session_state['Counter1'] = -1
 Counter2 = 3
 
 agg_ita1 = ['giovane', 'nuovo', 'aperto', 
