41733cf632dc092587ed668a44ca04605f22a9f7
PetraBattistata <127765772+PetraBattistata@users.noreply.github.com>
2023-05-14 12:08:30 +0200

    Project.py

--- Project.py
+++ Project.py
@@ -1,60 +1,59 @@
+from random import choice
+import json,requests
 import streamlit as st
-import json, requests
-import random
 from googletrans import Translator
 
-Counter1 = 3
-if 'Counter1' not in st.session_state:
-    st.session_state['Counter1'] = Counter1 -1
-    st.write(st.session_state.Counter1)
-    
-Counter2 = 3
+st.title('Adjectives')
 
-agg_ita1 = ['giovane', 'nuovo', 'aperto', 
-           'grande', 'caldo', 
-            'amichevole', 'alto', 'lungo' ]
+if 'counter1' not in st.session_state or 'counter2' not in st.session_state :
+ st.session_state.counter1 = 3
+ st.session_state.counter2 = 3
+if st.session_state.counter1 < 1 or st.session_state.counter2 < 1:
+ st.title('Game Over!')
+ st.stop()
 
-agg_ita2 = ['bagnato', 'conosciuto', 'pericoloso', 'sano', 'morbido', 
-           'pesante', 'pieno', 'veloce', 'facile'
-           'silenzioso', 'dolce']
+word_input = st.text_input('inserisci la tua traduzione: ','')
 
-agg_ita3 = ['nervoso', 'minuscolo', 'diligente', 'sincero', 'primo', 
-          'paziente', 'ordinato', 'presto', 'costoso',
-          'interessante', 'coraggioso', 'sazio' ]
 
+words = ['forte','alto','ricco']
+if 'choice' not in st.session_state:
+ st.session_state.choice = choice(words)
 translator = Translator()
+trans = translator.translate(st.session_state.choice,src='it', dest= 'en')
+if 'word_trans' not in st.session_state:
+ st.session_state.word_trans = trans.text
+ 
+def new_word(new_word):
+ st.session_state.choice = new_word
+ new_trans = translator.translate(new_word,src='it', dest= 'en')
+ st.session_state.word_trans = new_trans.text
 
-niveau = st.selectbox('Choose your Level!', ('A1', 'A2', 'B1'))
-var = ''
+ 
+st.write(st.session_state.choice)
 
+  
+if word_input:
+ if st.session_state.word_trans != word_input:
+  st.session_state.counter1 -=1
+  st.write('Sbagliato!')
+ elif st.session_state.word_trans == word_input:
+  st.write('Esatto! Ora passiamo agli antonimi!')
+  word_ant=st.text_input('scrivi qui l\' antonimo: ','')
+  url= 'https://api.datamuse.com/words?rel_ant=' + st.session_state.word_trans + ''
+  response = requests.get(url)
+  datamuse = json.loads(response.text)
+  if 'antonym' not in st.session_state:
+   st.session_state.antonym = datamuse[0]['word']
+  st.write(st.session_state.antonym)
+  if word_ant:
+   if word_ant != st.session_state.antonym:
+    st.session_state.counter2 -=1
+    st.write('Sbagliato!')
+   elif word_ant == st.session_state.antonym:
+    st.write('Esatto!')
+    new_word(choice(words))
+    st.write(st.session_state.choice)
 
-if niveau == 'A1': 
-           var = random.choice(agg_ita1)
-elif niveau == 'A2': 
-           var = random.choice(agg_ita2)
-elif niveau == 'B1': 
-           var = random.choice(agg_ita3)
-else:
-           pass
-st.write(var) 
+st.write(st.session_state.counter1)
+st.write(st.session_state.counter2)
 
-while Counter1 > 0:
-           st.write(Counter1)
-           Adj_cor = translator.translate(var,dest='en')
-           
-           Adj_eng = st.text_input('Tell me the name of this adjective in english!','')
-           
-           if Adj_eng:
-                      if Adj_eng != Adj_cor.text:
-                                 st.write('Try again!')
-                                 Counter1 -= 1
-                                 st.write(Counter1)
-                      elif Adj_eng.text == Adj_cor.text:
-                                 st.write('right!')
-                                 break
-if Counter1 == 0:
-           st.write('Game over!')
-              
-#'Now tell me the antonym of this word!' 
-           
-                      
